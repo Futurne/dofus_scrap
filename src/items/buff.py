7b852b6d99e4dbfd7cc83663cbd106470d9b2d5c
Pierre Pereira <pstmrtem@outlook.com>
2022-09-23 18:41:52 +0200

    feat: buff accepting buffs coming from some set

--- src/items/buff.py
+++ src/items/buff.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from copy import copy
+from src.preprocess.parse_json import VALID_BUFFS
+
+
+# Init VALID_BUFFS with some other buffs
+VALID_BUFFS = copy(VALID_BUFFS)
+VALID_BUFFS |= {'Spécial'}
+for element in ['Agilité', 'Force', 'Chance', 'Intelligence', 'Vitalité', 'Sagesse']:
+    VALID_BUFFS |= {f'{element} de base'}
+
+
 class Buff:
     def __init__(
         self,
@@ -9,6 +20,8 @@ class Buff:
     ):
         self.nom = nom
         self.poids = poids
+        
+        assert nom in VALID_BUFFS
 
     def __eq__(self, other: 'self') -> bool:
         return self.nom == other.nom and self.poids == other.poids
